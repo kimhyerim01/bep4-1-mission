74b4ab43fceefaed9818bd5840f6ede95b386e5c
김혜림 <khrim981209@gmail.com>
2025-12-23 11:56:33 +0900

    0002

--- src/main/java/com/back/initData/DataInit.java
+++ src/main/java/com/back/initData/DataInit.java
@@ -1,7 +1,9 @@
 package com.back.initData;
 
 import com.back.entity.Member;
+import com.back.entity.Post;
 import com.back.service.MemberService;
+import com.back.service.PostService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -12,18 +14,26 @@ import org.springframework.transaction.annotation.Transactional;
 @Configuration
 @Slf4j
 public class DataInit {
+
     private final DataInit self;
     private final MemberService memberService;
+    private final PostService postService;
 
-    public DataInit(@Lazy DataInit self, MemberService memberService) {
+    public DataInit(
+            @Lazy DataInit self,
+            MemberService memberService,
+            PostService postService
+    ) {
         this.self = self;
         this.memberService = memberService;
+        this.postService = postService;
     }
 
     @Bean
     public ApplicationRunner baseInitDataRunner() {
         return args -> {
             self.makeBaseMembers();
+            self.makeBasePosts();
         };
     }
 
@@ -31,13 +41,24 @@ public class DataInit {
     public void makeBaseMembers() {
         if (memberService.count() > 0) return;
 
-        Member systemMember = memberService.join("system", "1234", "시스템");
-        Member holdingMember = memberService.join("holding", "1234", "홀딩");
-        Member adminMember = memberService.join("admin", "1234", "관리자");
-        Member user1Member = memberService.join("user1", "1234", "유저1");
-        Member user2Member = memberService.join("user2", "1234", "유저2");
-        Member user3Member = memberService.join("user3", "1234", "유저3");
+        memberService.join("user1", "1234", "유저1");
+        memberService.join("user2", "1234", "유저2");
+        memberService.join("user3", "1234", "유저3");
+    }
+
+    @Transactional
+    public void makeBasePosts() {
+        if (postService.count() > 0) return;
+
+        Member user1 = memberService.findByUsername("user1").get();
+        Member user2 = memberService.findByUsername("user2").get();
+        Member user3 = memberService.findByUsername("user3").get();
+
+        postService.write(user1, "제목1", "내용1");
+        postService.write(user1, "제목2", "내용2");
+        postService.write(user1, "제목3", "내용3");
+        postService.write(user2, "제목4", "내용4");
+        postService.write(user2, "제목5", "내용5");
+        postService.write(user3, "제목6", "내용6");
     }
 }
-
-//빌드 다 되고 초반에 실행
