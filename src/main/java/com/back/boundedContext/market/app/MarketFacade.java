9d8f097921e1d45e7c19db8d6551066c6ac2b570
김혜림 <khrim981209@gmail.com>
2025-12-29 11:54:03 +0900

    0030

---
+++ src/main/java/com/back/boundedContext/market/app/MarketFacade.java
@@ -0,0 +1,20 @@
+package com.back.boundedContext.market.app;
+
+import com.back.boundedContext.market.domain.MarketMember;
+import com.back.shared.member.dto.MemberDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service    //
+@RequiredArgsConstructor
+public class MarketFacade {
+    private final MarketSyncMemberUseCase marketSyncMemberUseCase;
+
+    @Transactional
+    public MarketMember syncMember(MemberDto member) {
+        return marketSyncMemberUseCase.syncMember(member);
+    }
+}
+
+// 외부 방하벽 역할, 외부에서 호출되는 공식 진입점
